cd74ab7992112218f543c6ecb87ba492ee5c7d3c
Aman Shrivastava <aman.srivastava999@gmail.com>
2024-05-02 20:59:06 -0400

    Sampling from synmasks

--- image_sample.py
+++ image_sample.py
@@ -65,7 +65,7 @@ def main():
     logger.log("sampling...")
     # all_samples = []
     for i, (batch, cond) in enumerate(data):
-        import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
         image = None
         if batch is not None:
             image = ((batch + 1.0) / 2.0).cuda()
